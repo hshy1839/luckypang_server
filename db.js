249fd346cd87c46e286b81b8c944b6bef844fbc6
hshy1839 <hongjeongmin1839@gmail.com>
2025-04-07 14:35:42 +0900

    feat: mongoDB connect

--- db.js
+++ db.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    mongoose.connect('mongodb+srv://nuggiy:uNnrfEKHqxKEwBFT@cluster0s.kz9wc.mongodb.net/shopping_mall?retryWrites=true&w=majority&appName=Cluster0s', {
-      
-      tlsInsecure: true, // SSL/TLS 문제 해결
-      family: 4,         // IPv4 강제 사용
-    });
+    mongoose.connect(
+      'mongodb+srv://Ttang:luck0317%40%21@cluster0.ybwjowo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+      {
+        tlsInsecure: true,
+        family: 4,
+      }
+    );
     console.log('MongoDB Atlas 연결 성공!');
   } catch (err) {
     console.error('MongoDB Atlas 연결 실패:', err);
